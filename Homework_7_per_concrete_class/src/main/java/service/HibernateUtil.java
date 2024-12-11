580c2e65e09ec7d9564b3d4bfc36f9679195384c
sergey <oopp@gmail.com>
2024-12-11 22:21:05 +0300

    решена задача №7 с 3-ым вариантом (3 -  Table Per Concrete class mapping)

---
+++ Homework_7_per_concrete_class/src/main/java/service/HibernateUtil.java
@@ -0,0 +1,22 @@
+package service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY =
+            Persistence.createEntityManagerFactory("tasks");
+
+
+    public static EntityManager getEntityManager() {
+        return ENTITY_MANAGER_FACTORY.createEntityManager();
+    }
+
+    public static void close() {
+        ENTITY_MANAGER_FACTORY.close();
+    }
+
+
+}
